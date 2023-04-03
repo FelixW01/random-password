e39b6892be4564bc5786601250a9a1eb47933538
Felix W <felixwillem01@yahoo.com>
2023-04-03 16:18:22 -0400

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,38 @@
 // Assignment code here
 
+function generatePassword() {
+  
 
+  var characterList = {
+    special: ['!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'],
+    numeric: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    lowercase: ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  }
+
+  var characterTypes = {
+    length: Number(prompt("Enter a password length between 8 and 128")), 
+    // special: confirm("Click ok to confirm including special characters"),
+    numeric: confirm("Click ok to confirm including numeric characters"),
+    // lower: confirm("Click ok to confirm including lowercase characters"),
+    // upper: confirm("Click ok to confirm including uppercase characters")
+  };
+  var finalPassword = "";
+  for (var i = 0; i < characterTypes.length; i++) {
+    finalPassword += characterTypes.numeric(Math.floor(Math.random() * characterTypes.numeric.length));
+
+  }
+
+  // if (charSet.numeric === true) {
+  //   var newPassword= (characterList.numeric[Math.floor(Math.random()*characterList.numeric.length)]);
+  //   finalPassword.push(newPassword);
+  // }
+
+console.log(finalPassword);
+console.log(charSet.numeric);
+  console.log("Button clicked");
+  return finalPassword;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
